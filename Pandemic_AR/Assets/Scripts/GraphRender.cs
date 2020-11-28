9d6009fb168a64cc42f28392df4cd36a82abc3c7
Megan Smith <megansmith@MacBook-Pro.local>
2020-11-28 14:07:36 +0100

    unity latest prototype with new for Malik

--- Pandemic_AR/Assets/Scripts/GraphRender.cs
+++ Pandemic_AR/Assets/Scripts/GraphRender.cs
@@ -11,16 +11,20 @@ public class GraphRender : MonoBehaviour
     public Toggle LineGraphToggleUI;
     public Button FilterButtonUI;
     public Button ExitButtonUI;
-   
+
+   //global variables define visibility of graphs
     private Boolean BarGraph;
     private Boolean PieChart;
     private Boolean LineGraph;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Connects EventOnClick with the function to process this event
         FilterButtonUI.GetComponent<Button>().onClick.AddListener(FilterOnClickEvent);
         ExitButtonUI.GetComponent<Button>().onClick.AddListener(ExitOnClickEvent);
 
+        //Initializes global variables to the current states of the Toggles
         BarGraph = BarGraphToggleUI.GetComponent<Toggle>().isOn;
         PieChart = PieChartToggleUI.GetComponent<Toggle>().isOn;
         LineGraph = LineGraphToggleUI.GetComponent<Toggle>().isOn;
@@ -29,11 +33,13 @@ public class GraphRender : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Setting the visibility of the children to the current value of the Toggles
         gameObject.transform.GetChild(0).GetComponent<Renderer>().enabled = BarGraph;
         gameObject.transform.GetChild(1).GetComponent<Renderer>().enabled = PieChart;
         gameObject.transform.GetChild(2).GetComponent<Renderer>().enabled = LineGraph;
     }
 
+    //Saves the state of the Toggles at the moment of clicking on the Filter button
     void FilterOnClickEvent()
     {
         BarGraph = BarGraphToggleUI.GetComponent<Toggle>().isOn;
@@ -43,6 +49,7 @@ public class GraphRender : MonoBehaviour
 
     void ExitOnClickEvent()
     {
-
+        Application.Quit();
+        Debug.Log("You touched this button.");
     }
 }
