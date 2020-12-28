42a6cf29708346a92389730617a89d6c1e386346
Megan Smith <megansmith@MacBook-Pro.local>
2020-12-28 13:05:14 +0100

    version used for user test, without first sneeze video because it was too large to upload to git

---
+++ Pandemic_AR/Assets/Scripts/TextFaceCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextFaceCamera : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+
+    void Start()
+    {
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+
+        v.x = v.z = 0.0f;
+
+        transform.LookAt(cameraToLookAt.transform.position - v);
+
+        //transform.Rotate(Camera.main.transform.rotation);
+        transform.Rotate(0, 180, 0);
+    }
+
+}
---
+++ Pandemic_AR/Assets/Scripts/UITextOnClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UITextOnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("You touched this button.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("You touched this button.");
+    }
+}
